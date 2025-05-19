174772dcfce4bedb889085324965965c8d5ecebe
Alwa Dewidar <alidewidar23718@gmail.com>
2025-05-19 18:19:33 +0300

    add throws to method

--- src/main/java/com/pioneers/PicturePublishingService/service/picture/PictureService.java
+++ src/main/java/com/pioneers/PicturePublishingService/service/picture/PictureService.java
@@ -3,12 +3,13 @@ package com.pioneers.PicturePublishingService.service.picture;
 import com.pioneers.PicturePublishingService.model.dto.PictureDto;
 import com.pioneers.PicturePublishingService.model.response.PictureResponse;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 public interface PictureService {
 
-    PictureResponse uploadPicture(PictureDto request);
+    PictureResponse uploadPicture(PictureDto request) throws IOException;
 
     List<PictureResponse> getAllPendingPictures();
 
