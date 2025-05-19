c8e635e967dbbecc0f26b639cdd433753bc77c99
Alwa Dewidar <alidewidar23718@gmail.com>
2025-05-19 18:15:53 +0300

    add throws exception to method

--- src/main/java/com/pioneers/PicturePublishingService/service/fileStorage/FileStorageService.java
+++ src/main/java/com/pioneers/PicturePublishingService/service/fileStorage/FileStorageService.java
@@ -3,7 +3,9 @@ package com.pioneers.PicturePublishingService.service.fileStorage;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Service
 public interface FileStorageService {
-    String saveFile(MultipartFile file);
+    String saveFile(MultipartFile file) throws IOException;
 }
