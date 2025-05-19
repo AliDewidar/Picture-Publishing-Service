ecafc5aa37b0f920ba39af777be11d46b840ad73
Alwa Dewidar <alidewidar23718@gmail.com>
2025-05-19 18:04:38 +0300

    modify wrong message

--- src/main/java/com/pioneers/PicturePublishingService/error/exception/UserNotLoggedIn.java
+++ src/main/java/com/pioneers/PicturePublishingService/error/exception/UserNotLoggedIn.java
@@ -3,6 +3,6 @@ package com.pioneers.PicturePublishingService.error.exception;
 public class UserNotLoggedIn extends BaseCustomException {
 
     public UserNotLoggedIn(String description) {
-        super(1004, "Uses not logged in", description);
+        super(1004, "User not logged in", description);
     }
 }
