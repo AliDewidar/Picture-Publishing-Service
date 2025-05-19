7d0487332df11c1c50e583d3c2c8ce55b56dd7e2
Alwa Dewidar <alidewidar23718@gmail.com>
2025-05-19 18:03:37 +0300

    add custom constructor

--- src/main/java/com/pioneers/PicturePublishingService/error/exception/FileStorageException.java
+++ src/main/java/com/pioneers/PicturePublishingService/error/exception/FileStorageException.java
@@ -2,6 +2,10 @@ package com.pioneers.PicturePublishingService.error.exception;
 
 public class FileStorageException extends BaseCustomException {
 
+    public FileStorageException(String description) {
+        super(1008, "File storage error", description);
+    }
+
     public FileStorageException(String description, Throwable cause) {
         super(1008, "File storage error", description, cause);
     }
