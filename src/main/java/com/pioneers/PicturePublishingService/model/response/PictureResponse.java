cd547ec2b0344e8a4f8174193a11c35b4930367c
Alwa Dewidar <alidewidar23718@gmail.com>
2025-05-19 18:07:34 +0300

    convert class to record

--- src/main/java/com/pioneers/PicturePublishingService/model/response/PictureResponse.java
+++ src/main/java/com/pioneers/PicturePublishingService/model/response/PictureResponse.java
@@ -1,19 +1,9 @@
 package com.pioneers.PicturePublishingService.model.response;
 
 import com.pioneers.PicturePublishingService.model.enums.Category;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
-
 import java.util.UUID;
 
-@Value
 @Builder
-@AllArgsConstructor
-public class PictureResponse {
-    UUID id;
-    String userEmail;
-    String description;
-    Category category;
-    String url;
+public record PictureResponse(UUID id, String userEmail, String description, Category category, String url) {
 }
